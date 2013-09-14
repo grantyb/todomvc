facdfc8445cb5aca00964ff75f7ead51b9f19ec4
Grant Berridge <grant@cactuslab.com>
2013-09-14 14:02:03 +1200

    Copied from Knockout's example, and started refactoring for Consistent.js

---
+++ architecture-examples/consistentjs/bower_components/consistent.js/jquery.consistent.js
@@ -0,0 +1,79 @@
+/*! 
+ * Consistent.js jQuery plugin 0.8
+ * @author Karl von Randow
+ * @license Apache License, Version 2.0
+ */
+ /*
+    Copyright 2013 Karl von Randow
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+  */
+
+(function($, window, undefined) {
+	var Consistent = $.consistent = window.Consistent;
+
+	/**
+	 * Argument scenarios:
+	 * 1) $(...).consistent(): create a new scope, bind the selected nodes and return the new scope
+	 * 2) $(...).consistent(aScope): bind the selected nodes for the given scope and return the scope
+	 * 3) $(...).consistent(options): create a new scope with the given options, bind the selected nodes and return the new scope.
+	 * 4) $(...).consistent(aScope, options): bind the selected nodes for the given scope with the given options and return the scope.
+	*/
+	$.fn.consistent = function() {
+		var scope, options;
+		if (arguments.length === 0) {
+			/* Check whether the given nodes have an existing scope */
+			this.each(function() {
+				var nodeScope = Consistent.findScopeForNode(this);
+				if (nodeScope) {
+					if (scope && scope !== nodeScope) {
+						/* The given nodes have multiple different scopes */
+						throw "The given nodes have multiple different scopes";
+					}
+					scope = nodeScope;
+				} else if (scope) {
+					/* Some of the given nodes have scopes and others don't */
+					throw "Some of the given nodes have scopes and others don't";
+				}
+			});
+
+			if (scope) {
+				/* Already bound to a scope */
+				return scope;
+			} else {
+				/* Create a new default scope */
+				scope = Consistent();
+			}
+		} else {
+			var arg0 = arguments[0];
+			if (Consistent.isScope(arg0)) {
+				/* Scope and maybe options */
+				scope = arg0;
+				options = arguments.length > 1 ? arguments[1] : null;
+			} else if (typeof arg0 === "object") {
+				/* Options */
+				scope = Consistent(arg0);
+				options = null;
+			} else {
+				throw "First argument to $.consistent was not an appropriate type: " + typeof arg0;
+			}
+		}
+
+		this.each(function() {
+			scope.$.bind(this, options);
+		});
+
+		return scope;
+	};
+
+})(jQuery, window);
---
+++ architecture-examples/consistentjs/readme.md
@@ -0,0 +1,17 @@
+# Consistent.js TodoMVC Example
+
+> Consistent is a small and simple Javascript framework to enable an abstract model to be synced with the DOM.
+
+> _[Consistent.js - github.com](https://github.com/karlvr/consistent.js)_
+
+
+## Learning Consistent.js
+
+The [README](https://github.com/karlvr/consistent.js/blob/master/README.md) is a great resource for getting started.
+
+_If you have other helpful links to share, or find any of the links above no longer work, please [let us know](https://github.com/tastejs/todomvc/issues)._
+
+
+## Credit
+
+This TodoMVC application was originally created by [grantyb](https://github.com/grantyb).
